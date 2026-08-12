7d66baf25f642f4f85e4e76d36b786ae59993a72
befogfang <kalyantthakur@gmail.com>
2026-08-12 23:58:57 +0530

    uppercase or not

--- Ph1_01_l1_simpleCondition/10_n.js
+++ Ph1_01_l1_simpleCondition/10_n.js
@@ -1,1 +1,26 @@
 // take  a character and check weather if it's uppercase , lowercase , a digit or a special character
+
+/**
+ * uppercase --> 65 - 90
+ * lowercase --> 97 - 122
+ * digit --> 48- 57  
+ * special characterz -->32-space ,..., 47 - / ,, 58 - 64 ,, 123 -126
+ * 
+ * function used here --> .fromCharCode(ascii value)
+ * 
+ */
+
+function tellWhatsthis(s){
+
+let value = s.charCodeAt(0)
+console.log(value)
+
+if (value>65 && value<90){
+    console.log("Uppercase")
+} else {
+    console.log("not uppercase")
+}
+    
+}
+
+tellWhatsthis("t")
