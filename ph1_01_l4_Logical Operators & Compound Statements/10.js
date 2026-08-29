dad01c2f79c0fda5a5049ef61d998753809df7a3
befogfang <kalyantthakur@gmail.com>
2026-08-29 12:09:08 +0530

    password validation as per security code

--- ph1_01_l4_Logical Operators & Compound Statements/10.js
+++ ph1_01_l4_Logical Operators & Compound Statements/10.js
@@ -1,37 +1,62 @@
-//Take a password string and check basic rules (length $\ge$ 8 and contains at least one digit).  
+//Take a password passwording and check basic rules (length $\ge$ 8 and contains at least one digit).  
 
 function checkpassword(password) {
 
     checkPWlength(password)
 
-    function instructions() {
-        console.log( "follow the instructions");   
+    function inpassworductions() {
+        console.log("follow the inpassworductions");
     }
 
     //check password length
     function checkPWlength(password) {
-        let pwSlen = password.length ;
-        (pwSlen>7)?checkForCharacters(password):instructions()
+        let pwSlen = password.length;
+        (pwSlen > 7) ? checkForCharacters(password) : inpassworductions()
     }
+
     function checkForCharacters(password) {
-        let pString = password.toString().split('')
-        pString.forEach(c=> {
-            let asC = c.charCodeAt(0);
-            if (asC>=65&&asC<90) {
-                console.log("it has upper case ")
-            }else if (asC>=97&&asC<123) {
-                console.log("it has lower case ")
-            }  else if (asC>=48&&asC<57) {
-                console.log("it has digits ")
-            }  else{
-                
-            }
-        });
+
+        const hasLetter = /[a-zA-Z]/.test(password);
+        const hasDigit = /[0-9]/.test(password);
+        const hasSymbol = /[^a-zA-Z0-9\s]/.test(password);
+
+        if (hasLetter && hasDigit && hasSymbol) {
+            console.log("Contains letters, digits, and symbols");
+            checkConq(password)
+        } else {
+            inpassworductions()
+        }
     }
+
+    function checkConq(password) {
     
+// No 3+ identical chars in a row
+function hasRepeated(password) {
+  return /(.)\1\1/.test(password);
 }
 
-checkpassword("13423586")
+// No 3+ sequential chars (abc, 123, cba, 321, ...)
+function hasSequential(password) {
+  for (let i = 0; i < password.length - 2; i++) {
+    const a = password.charCodeAt(i);
+    const b = password.charCodeAt(i + 1);
+    const c = password.charCodeAt(i + 2);
+    if ((b - a === 1 && c - b === 1) || (a - b === 1 && b - c === 1))
+      return true;
+  }
+  return false;
+}
+
+if (hasRepeated(password) || hasSequential(password)) {
+  console.log("Invalid: cann't used as the password as contains consecutive characters");
+} else {
+  console.log("Valid and u can use this password , pls save this password somewhere carefully");
+}   
+
+}
+}
+
+checkpassword("1Abc@df586")
 // checkpassword("123458495236")
 // checkpassword("1234586asdf")
 // checkpassword("1234586@##asdf")
