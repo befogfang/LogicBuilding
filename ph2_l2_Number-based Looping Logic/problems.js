4838677bbffaabde7181929f7b02fb97c8fbb8e8
befogfang <kalyantthakur@gmail.com>
2026-09-02 12:37:44 +0530

    34534

---
+++ ph2_l2_Number-based Looping Logic/problems.js
@@ -0,0 +1,29 @@
+// Count the number of digits in a given number.  
+function countTheDigit(number) {
+    let dLength = number.toString().length
+    console.log( dLength)
+}
+// countTheDigit(34324)
+
+// Print the reverse of a given number.  
+function reverseNumber(number) {
+    let r = number.toString()
+    console.log(r)
+}
+reverseNumber(342342)
+
+// Check if a number is a palindrome.  
+
+// Find the sum of digits of a number.  
+
+// Check if a number is an Armstrong number.  
+
+// Check if a number is a perfect number.  
+
+// Print all prime numbers between 1 and 100.  
+
+// Check if a number is prime or not.  
+
+// Print Fibonacci series up to n terms.  
+
+// Print sum of first n terms of Fibonacci series.
